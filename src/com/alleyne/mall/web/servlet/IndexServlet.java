8a211da4f917a09dc364ed2904fd7d948bc27145
Alleyne <lindongyue@mnano.org>
2019-10-09 21:08:38 +0800

    1.增加编码过滤器。 2.增加bean工厂工具类。 3.增加随机码工具类

---
+++ src/com/alleyne/mall/web/servlet/IndexServlet.java
@@ -0,0 +1,13 @@
+package com.alleyne.mall.web.servlet;
+
+import com.alleyne.mall.web.servlet.base.BaseServlet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IndexServlet extends BaseServlet {
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        return null;
+    }
+}
--- web/index.jsp
+++ web/index.jsp
@@ -1,10 +1,3 @@
-<%-- Created by IntelliJ IDEA. --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title></title>
-  </head>
-  <body>
-
-  </body>
-</html>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+         pageEncoding="UTF-8"%>
+<jsp:forward page="/IndexServlet"></jsp:forward>
